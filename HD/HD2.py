70a0aa7eea704d7d10a3278967e3c84eb252bc84
Pedro-Muller29 <pedro.mezzacasa@gmail.com>
2022-10-19 10:38:14 -0300

    versao final

--- HD/HD2.py
+++ HD/HD2.py
@@ -1,10 +1,10 @@
 with open('input.txt') as f:
-    data = [[int(y) for y in x.split(",")] for x in f.readlines()]
-
-data = data[0]
-
+    data = [[int(ord(y)) for y in x] for x in f]
+data = data[0] + [17, 31, 73, 47, 23]
 
 lista = [x for x in range(256)]
+
+
 
 
 def inverter_Sublista(idx, comp):
@@ -32,16 +32,36 @@ def inverter_Sublista(idx, comp):
     novo_subinicio = aux[len(sub_fim):]
     lista = novo_subinicio + lista[idx_inicio: idx]+ novo_subFim + lista[idx_fim:]
         
-
 idx = 0
 salto = 0
+for l in range(64):
+    print()
+    print(l)
+    
+    for i in data:
+        inverter_Sublista(idx, i)
+        if i+salto == len(lista):
+            pass
+        else:
+            idx = (idx+i+salto) % len(lista)
+        salto += 1
+    print(salto)
 
-for i in data:
-    inverter_Sublista(idx, i)
-    if i+salto == len(lista):
-        pass
+def xor(listaa):
+    tmp = 0
+    for i in listaa:
+        tmp = tmp ^ i
+    return tmp
+hash = []
+for t in range(16):
+    hash.append(xor(lista[t*16:(t+1)*16]))
+
+resposta = ""
+for ha in hash:
+    if ha < 16:
+        tmpha = "0" + hex(ha)[2:]
     else:
-        idx = (idx+i+salto) % len(lista)
-    salto += 1
+        tmpha = hex(ha)[2:]
+    resposta = resposta + tmpha
 
-print(lista[0] * lista[1])
+print(resposta)
