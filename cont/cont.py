caa02e0b8d3840eeb08679c59920ef2dfc55bb26
Pedro-Muller29 <pedro.mezzacasa@gmail.com>
2022-11-01 17:56:09 -0300

    Update cont.py

--- cont/cont.py
+++ cont/cont.py
@@ -1,21 +1,20 @@
 x = 1321131112
 def fazernum(num):
-    list_num = [int(x) for x in str(num)]
     """
     pega a sequência de numeros iguais
     """
-    fin_num = ""
+    tmp = ""
     cont = 1
     ultnum = None
-    for n in list_num:
+    for n in str(num):
         if n == ultnum:
             cont += 1
         else:
             if ultnum is not None:
-                fin_num = fin_num + str(cont) + str(ultnum)
+                tmp += str(cont) + ultnum
             cont = 1
             ultnum = n
-    fin_num = fin_num + str(cont) + str(ultnum)
+    fin_num = tmp + str(cont) + ultnum
     return fin_num
 
 for i in range(50):
